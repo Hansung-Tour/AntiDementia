9d2a8138e79179c31cc8edf1a76806731ff5b43e
KIMYERIM <ddr2038@naver.com>
2021-05-26 22:54:17 +0900

    commit message

--- app/src/main/java/com/hansungtour/antidementia/fragment/Question9Fragment.kt
+++ app/src/main/java/com/hansungtour/antidementia/fragment/Question9Fragment.kt
@@ -5,57 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
+
 
 import com.hansungtour.antidementia.R
+import kotlinx.android.synthetic.main.fragment_question5.* //...fragment_question**Number**.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class Question9Fragment : Fragment(), View.OnClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Question9Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Question9Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question9, container, false)
+        return inflater.inflate(R.layout.fragment_question9, container, false) //fragment_question 수정
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Question9Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Question9Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        op1.setOnClickListener(this)
+        op2.setOnClickListener(this)
+        op3.setOnClickListener(this)
+        next.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) { //0==오답, 1==정답, score[Number] Number가 0인 경우 1번문제
+            R.id.op1->{score[3] = 0}
+            R.id.op2->{score[3] = 0} //이 문제 같은 경우 여기가 정답
+            R.id.op3->{score[3] = 1}
+            R.id.next->{navController.navigate(R.id.action_question9Fragment_to_question10Fragment)}
+        }
     }
 }
+
